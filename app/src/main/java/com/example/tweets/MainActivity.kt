479aa1620875ede6a5f809fc8f8e3ef3556e3a12
Umairhanif <umair.hanif@hazelmobile.co>
2024-08-23 16:37:15 +0500

    loading added

--- app/src/main/java/com/example/tweets/MainActivity.kt
+++ app/src/main/java/com/example/tweets/MainActivity.kt
@@ -5,11 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -24,12 +31,23 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TweetsTheme {
-                App()
+                Scaffold(
+                    topBar = {
+                        TopAppBar(title = {
+                            Text(text = "Tweets")
+                        }, colors = TopAppBarDefaults.topAppBarColors())
+                    }
+                ) {
+                    Box(modifier = Modifier.padding(it)){
+                        App()
+                    }
+                }
             }
         }
     }
@@ -52,26 +70,4 @@ fun App() {
             DetailScreen()
         }
     }
-}
-
-@Composable
-fun RegistrationScreen(onClick:(Pair<String,Int>)->Unit) {
-    Text(
-        text = "Registration Screen",
-        style = MaterialTheme.typography.headlineLarge,
-        modifier = Modifier
-            .clickable {
-                onClick(Pair("umairhanif277", 814))
-            }
-            .padding(50.dp))
-}
-
-@Composable
-fun LoginScreen() {
-    Text(text = "Login Screen", style = MaterialTheme.typography.headlineLarge)
-}
-
-@Composable
-fun MainScreen(argument: String?, password: Int?) {
-    Text(text = "Main Screen $argument $password", style = MaterialTheme.typography.headlineLarge)
 }
--- app/src/main/java/com/example/tweets/ui/screens/DetailsScreen.kt
+++ app/src/main/java/com/example/tweets/ui/screens/DetailsScreen.kt
@@ -1,6 +1,8 @@
 package com.example.tweets.ui.screens
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -10,6 +12,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -21,10 +24,16 @@ import com.example.tweets.viewmodels.DetailViewModel
 fun DetailScreen() {
     val detailsViewModel:DetailViewModel= hiltViewModel()
     val tweet=detailsViewModel.tweets.collectAsState()
-    LazyColumn {
-        items(tweet.value){
-            TweetListItem(tweet = it.text)
+    if (tweet.value.isEmpty()){
+        Box(modifier = Modifier.fillMaxSize(1f), contentAlignment = Alignment.Center){
+            Text(text = "Loading...")
+        }
+    }else{
+        LazyColumn {
+            items(tweet.value){
+                TweetListItem(tweet = it.text)
 
+            }
         }
     }
 }
